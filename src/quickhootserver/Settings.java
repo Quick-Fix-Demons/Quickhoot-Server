6ea9df89a3b0330a64cd4651b391a58eb2564678
ok_google <renda855@gmail.com>
2020-11-12 10:56:28 +0100

    settings class

---
+++ src/quickhootserver/Settings.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quickhootserver;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alessio
+ */
+public class Settings {
+    private ArrayList<String> domande;
+    private int port;
+    
+}
