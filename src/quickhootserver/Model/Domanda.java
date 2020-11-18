86082bfc586b967a100e99ca5e99539c02363fc6
Tolve <tolveandrea943@gmail.com>
2020-11-18 11:25:14 +0100

    per far funzionare il git pull

--- src/quickhootserver/Model/Domanda.java
+++ src/quickhootserver/Model/Domanda.java
@@ -10,5 +10,29 @@ package quickhootserver.Model;
  * @author andrea
  */
 public class Domanda {
+    private String domanda;
+    private int tempo=60;
+    
+    public Domanda(String d, int t){
+        this.domanda=d;
+        //this.tempo=t;
+    }
+
+    public String getDomanda() {
+        return domanda;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    public void setDomanda(String domanda) {
+        this.domanda = domanda;
+    }
+
+    public void setTempo(int tempo) {
+        this.tempo = tempo;
+    }
+    
     
 }
